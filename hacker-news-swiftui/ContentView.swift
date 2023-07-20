1bf084b1cf3171d42bb7a6e557d8ef436fb82426
bagusrizky15 <mohamad.15@students.amikom.ac.id>
2023-07-20 13:58:42 +0700

    Initial Commit

---
+++ hacker-news-swiftui/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  hacker-news-swiftui
+//
+//  Created by bagus on 20/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hacker-news-swiftui/hacker_news_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  hacker_news_swiftuiApp.swift
+//  hacker-news-swiftui
+//
+//  Created by bagus on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct hacker_news_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
